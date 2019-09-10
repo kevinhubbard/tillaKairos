a7c823292828f78b77fdb805135a6b330701dd80
Kevin Hubbard <dgkallday98@gmail.com>
2019-09-10 19:03:32 -0400

    jquery player

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -3,11 +3,27 @@ init();
 
 function init(){
 	var currentSong = 0;
-	$('#audio')[0].src = $('#playlist li a')[0];
+
+	$('#audio').attr('src', $('#playlist li a')[0]);
+
 	$('#playlist li a').click(function(e){
 		e.preventDefault();
-		$('#audio')[0].src = this;
+		$('#audio').attr('src', this);
 		$('#audio')[0].play();
+		$('#playlist li').removeClass('current-song');
+		currentSong = $(this).parent().index();
+		$(this).parent().addClass('current-song')
 	});
+
+	$('#audio')[0].addEventListener('ended', function(){
+		currentSong++;
+		if(currentSong == $('#playlist li a').length){
+			currentSong = 0;
+		}
+		$('#playlist li').removeClass('current-song');
+		$('#playlist li:eq('+currentSong+')').addClass('current-song');
+		$('#audio').attr('src', $('#playlist li a')[currentSong].href);
+		$('#audio')[0].play();
+	})
 	
 }	
